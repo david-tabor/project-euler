f1abe964a2d8347b1c8d7c5841738dfc25cbbd05
David Tabor <dtabor3@gmail.com>
2018-12-26 17:41:09 -0500

    created findFactors function in pe-12

--- pe-problem-12.js
+++ pe-problem-12.js
@@ -1,5 +1,4 @@
-// Computes 3.5 million primes in 15 seconds.  Largest prime found
-// will be 59,999,999.
+
 
 // Import dependenceies
 var present = require('present');
@@ -7,37 +6,107 @@ var present = require('present');
 // Record starting time for performance measurement.
 let t0 = present();
 
-// Initialize variables
-let max = 60000000; // Maximum number to check if prime.
-let sieve = new Array(max);
 
-// Initialize sieve array
-for (var i=0; i<=max; i++) {
-    sieve[i]=1;
+// Define function for computing prime number list
+function primeList() {
+    // Computes 3.5 million primes in 15 seconds.  Largest prime found
+    // will be 59,999,999.
+
+    // Initialize variables
+    let max = 60000000; // Maximum number to check if prime.
+    let a = new Array(max);
+    let out = []
+
+    // Initialize sieve array
+    for (var i=0; i<=max; i++) {
+        a[i]=1;
+    }
+
+    // Mark all composite numbers
+    for (var i=2; i<=max; i++) {
+        for (var j=2; i*j<=max; j++) {
+            a[i*j]=0;
+        }
+    }
+
+    // Generate output list of prime numbers
+    for (var i=2; i<=max; i++) {
+        if (a[i]==1) {
+            out.push(i)
+        }
+    }
+
+    return out;
 }
 
-// Mark all composite numbers
-for (var i=2; i<=max; i++) {
-    for (var j=2; i*j<=max; j++) {
-         sieve[i*j]=0;
+// Define function for factorizing a number given a prime list
+function findFactors(n, myPrimeList) {
+    // Returns all factors of n, excluding 1 and n, unless additional
+    // prime factors exist that are not present in myPrimeList, in
+    // which case an empty list will be returned.
+
+    // Initialize variables
+    var out = [];
+    var thisPrime = 0;
+    var checkProd = 1;
+
+    // Iterate through provided list of primes, pushing any 
+    // factors to output list
+    for (var i=0; i< myPrimeList.length; i++) {
+        thisPrime = myPrimeList[i];
+
+        // Exit for loop if next prime to check is bigger than n
+        if (thisPrime > n) {
+            break;
+        }
+
+        // Check for divisibility of thisPrime
+        if (n % thisPrime == 0) {
+            out.push(thisPrime);
+        }
+    }
+
+    // Check if n has been fully factorized and return factors if so
+    for (var i=0; i<out.length; i++) {
+        checkProd = checkProd * out[i];
+    }
+    if (checkProd==n){
+        return out;
+    }
+    else {
+        return [];
     }
 }
 
-// Count number of primes found
-var i_max = 2;
-let n = 0;
-for (var i=2; i<= max; i++){
-    if (sieve[i]==1) {
-        n++;
-        i_max = i;
-    }
-}
+// Define function for computing triangular number list
+
+
+
+//Main
+primes = primeList();
+//console.log(primes.slice(0,10))
+
+var factors = findFactors(29*2*11*13, primes)
+//console.log(factors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Compute execution time
 let t1 = present();
 let t_delta = Math.round((t1-t0)/1000.0);
-
-// Output calcuation results
-console.log("Calculated first " + n.toString() + " primes. ")
-console.log(n.toString() + "th prime = " + i_max.toString())
 console.log("Computation took " + t_delta.toString() + " seconds.")
