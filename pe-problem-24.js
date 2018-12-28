bcba1177b23e28b13a79124210fb432af8e7ed01
David Tabor <dtabor3@gmail.com>
2018-12-28 18:45:34 -0500

    naive iteration too slow, need new approach

--- pe-problem-24.js
+++ pe-problem-24.js
@@ -7,30 +7,13 @@ function isPerm(n) {
     var uniqueDigits;
 
     // If n is 8 digits or less, or 11 digits or more, return false
-    if (n<=99999999 || n>=1e11) {
-        
-        // // Debug msg
-        // console.log('Returning false bc <=8 digits or >=11 digits');
-        
-        return false;
-    }
-
+    if (n<=99999999 || n>=1e11) {return false;}
 
     // If n is 9 digits, add "leading zero" to digits list
-    if (n<=999999999) {
-        
-        // // Debug msg
-        // console.log('Adding leading zero to digits list.')
-        
-        digits.push(0);
-    }
+    if (n<=999999999) {digits.push(0);}
 
     // Populate "digits" with list of digits from n
     while (remainder>0) {
-        
-        // // Debug msg
-        // console.log('Starting next iteration. remainder,digits =', remainder, digits);
-        
         // Push ones digit of "remainder" to list of digits 
         digits.push(remainder % 10);
 
@@ -40,10 +23,6 @@ function isPerm(n) {
 
     // Generate Array of unique values in 'digits'
     uniqueDigits = Array.from(new Set(digits)); 
-
-    // Debug msg
-    console.log('digits =', digits);
-    console.log('uniqueDigits = ', uniqueDigits);
 
     // Check if "uniqueDigits" is same length as "digits"
     if (uniqueDigits.length == digits.length && digits.length == 10) {
@@ -59,19 +38,13 @@ var counter = 0;
 // Iterate through all possible 10 digit numbers in ascending order
 // checking if their order generates a valid permutation of 0-9.
 for (var i =123456789; i<=98765432210; i++) {
-    break;
+    if (isPerm(i)) {
+        counter += 1;
+        if (counter == 100000) {
+            console.log('reached counter=1000 at i=', i)
+            break;
+        }
+    }
 }
-
-
-console.log('Running isPerm() on 123456789.  Should return true.')
-console.log(isPerm(123456789));
-
-console.log(isPerm(1234567809));
-console.log(isPerm(1234567909));
-
-
-
-
-
 
 
