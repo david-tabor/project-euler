afe273faee8553b35f6346a8e3d7ffcf062cb59c
David Tabor <dtabor3@gmail.com>
2018-12-29 18:58:18 -0500

    started problem 26

---
+++ pe-problem-26.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+// Define function to check for repeating decimals from a string
+function FindRepeating(myString, minCycleLength){
+    len = myString.length;
+
+    
+}
+
+
+
+// Generate string representation of decimal expression
+var s = '';
+for (var i=2; i<11; i++) {
+    console.log('1/', i, ' ',  (1/i).toFixed(15));
+    var f = (1/i).toFixed(15);
+    s = f.toString();
+    s = s.slice(2,s.length-1);
+    console.log(s);
+}
+
+
